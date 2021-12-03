fb56206f7a426fdab4905bfd8d1b68dd897f6d17
LepakBoy <fikrinadzif35@gmail.com>
2021-12-04 01:58:25 +0700

    payment screen

--- src/screen/Payment/index.js
+++ src/screen/Payment/index.js
@@ -13,11 +13,112 @@ import {
 
 function Payment() {
   return (
-    <ScrollView>
-      <View>
-        <Text>payment</Text>
+    <>
+      <View style={s.totalPayment}>
+        <Text style={s.textTotal}>Total Payment</Text>
+        <Text style={s.textAmount}>$30</Text>
       </View>
-    </ScrollView>
+
+      <ScrollView style={s.wrapper}>
+        <Text style={s.label}>Payment Method</Text>
+        <View style={s.paymentContent}>
+          <View style={s.cardMethod}>
+            <Image
+              style={s.imgCard}
+              source={require('../../assets/images/dana.png')}
+            />
+          </View>
+          <View style={s.cardMethod}>
+            <Image
+              style={s.imgCard}
+              source={require('../../assets/images/google-pay.png')}
+            />
+          </View>
+          <View style={s.cardMethod}>
+            <Image
+              style={s.imgCard}
+              source={require('../../assets/images/paypal.png')}
+            />
+          </View>
+          <View style={s.cardMethod}>
+            <Image
+              style={s.imgCard}
+              source={require('../../assets/images/bca.png')}
+            />
+          </View>
+          <View style={s.cardMethod}>
+            <Image
+              style={s.imgCard}
+              source={require('../../assets/images/gopay.png')}
+            />
+          </View>
+          <View style={s.cardMethod}>
+            <Image
+              style={s.imgCard}
+              source={require('../../assets/images/visa.png')}
+            />
+          </View>
+          <View style={{width: '100%'}}>
+            <Text style={s.or}>Or</Text>
+            <Text style={[s.or, {fontSize: 16, fontWeight: '600'}]}>
+              Pay via cash. See how it work
+            </Text>
+          </View>
+        </View>
+        <Text style={s.label}>Personal Info</Text>
+        <View style={s.personalContent}>
+          <Text style={s.labelPersonal}>Full Name</Text>
+          <TextInput
+            placeholder="Type your name"
+            defaultValue=""
+            style={s.inputText}
+          />
+          <Text style={[s.labelPersonal, {marginTop: 18}]}>Email</Text>
+          <TextInput
+            placeholder="Type your email"
+            defaultValue=""
+            style={s.inputText}
+          />
+          <Text style={[s.labelPersonal, {marginTop: 18}]}>Phone Number</Text>
+          <TextInput
+            placeholder="Type your phone number"
+            defaultValue=""
+            style={s.inputText}
+          />
+          <View style={s.warn}>
+            <Image source={require('../../assets/images/warn.png')} />
+            <Text style={s.textWarn}>Fill your data correctly</Text>
+          </View>
+        </View>
+        <View style={s.btnArea}>
+          <TouchableOpacity style={s.btnPay}>
+            <Text style={s.textPay}>Pay your order</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={s.footer}>
+          <Image source={require('../../assets/images/Tickitz.png')} />
+          <Text style={[s.textGrey, {marginTop: 12}]}>
+            Stop waiting in line. Buy tickets conveniently, watch movies
+            quietly.
+          </Text>
+          <Text style={[s.labelFooter, {marginTop: 28}]}>Our Sponsor</Text>
+          <View style={s.logoArea}>
+            <Image
+              style={[{marginHorizontal: 8}, s.logo]}
+              source={require('../../assets/images/cineone.png')}
+            />
+            <Image
+              style={[{marginHorizontal: 8}, s.logo]}
+              source={require('../../assets/images/hiflix.png')}
+            />
+            <Image
+              style={[{marginHorizontal: 8}, s.logo]}
+              source={require('../../assets/images/ebv.png')}
+            />
+          </View>
+        </View>
+      </ScrollView>
+    </>
   );
 }
 export default Payment;
--- src/screen/Payment/style.js
+++ src/screen/Payment/style.js
@@ -1,6 +1,145 @@
 import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
-  paddingVertical: 28,
-  paddingHorizontal: 12,
+  wrapper: {
+    backgroundColor: 'white',
+    paddingVertical: 28,
+    paddingHorizontal: 12,
+    // backgroundColor: 'blue',
+    height: 4000,
+  },
+  totalPayment: {
+    paddingHorizontal: 12,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    // backgroundColor: 'white',
+    paddingVertical: 12,
+    // paddingHorizontal: 10,
+  },
+  label: {
+    color: '#14142B',
+    fontSize: 18,
+    fontWeight: '600',
+  },
+  subLabel: {
+    color: '#000000',
+    fontSize: 16,
+  },
+  textTotal: {
+    color: '#AAAAAA',
+    fontSize: 18,
+  },
+  textAmount: {
+    color: '#14142B',
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  paymentContent: {
+    justifyContent: 'space-between',
+    alignContent: 'space-around',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    padding: 22,
+    marginVertical: 22,
+    backgroundColor: 'white',
+    borderRadius: 8,
+    height: 220,
+  },
+  cardMethod: {
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 6,
+    height: 42,
+    // marginTop: 12,
+    width: '32%',
+    backgroundColor: 'white',
+    borderColor: '#DEDEDE',
+    borderWidth: 1,
+  },
+
+  imgCard: {
+    resizeMode: 'stretch',
+    // width: '90%',
+    height: 22,
+  },
+  or: {
+    marginTop: 16,
+    textAlign: 'center',
+  },
+  personalContent: {
+    padding: 22,
+    backgroundColor: 'white',
+    borderRadius: 8,
+    height: 400,
+  },
+  labelPersonal: {
+    color: '#696F79',
+    fontSize: 16,
+  },
+  inputText: {
+    marginTop: 12,
+    borderRadius: 6,
+    paddingLeft: 8,
+    backgroundColor: 'white',
+    width: '100%',
+    borderColor: '#DEDEDE',
+    borderWidth: 1,
+  },
+  warn: {
+    marginTop: 22,
+    backgroundColor: 'rgba(244, 183, 64, 0.3)',
+    height: 48,
+    paddingHorizontal: 22,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textWarn: {
+    marginLeft: 22,
+    color: '#4E4B66',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  btnArea: {
+    justifyContent: 'center',
+    height: 150,
+    marginTop: 22,
+  },
+  btnPay: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#5F2EEA',
+    height: 52,
+    borderRadius: 8,
+  },
+  textPay: {
+    color: 'white',
+    fontSize: 14,
+    fontWeight: '500',
+  },
+  footer: {
+    marginTop: 28,
+    width: '100%',
+    height: 400,
+    backgroundColor: 'white',
+  },
+  textGrey: {
+    color: '#6E7191',
+    fontSize: 14,
+  },
+  labelFooter: {
+    color: '#000000',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  logoArea: {
+    marginTop: 12,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  logo: {
+    height: 24,
+    resizeMode: 'stretch',
+  },
 });
